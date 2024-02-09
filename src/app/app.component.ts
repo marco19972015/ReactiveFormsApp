3e273fe5bb1f16ed336f551d730f321476afbecf
Marco Torres <marcoantonio.torres@icloud.com>
2024-02-09 14:33:06 -0500

    In version 4 the goal was to be able to access the values in the now NgForm object. To do this I first added the name property to every control of the form (control reffering to the inputs), then I added the use of ngModel to tell angular the input element is a control for the form. This allows us to check in dev tool and see the new properties that we are able to access. In the TS file we can check what's inside the form using the form value property through dot notation, or control value property through bracket notation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,34 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'template-driven-form';
 
-  // Using ViewChild allows us to access a DOM element or a component from the template
-  // In this case I use it to access the reference template variable which holds the form as an object of type NgForm
   @ViewChild('registrationForm') form!: NgForm
 
   onFormSubmitted(){
-    // reference the form variable 
     console.log(this.form);
+    // Accessing the values in the form 
+    console.log(this.form.control.value['firstName']);  // Instead of dot notation we use square bracket notation
+    console.log(this.form.value.lastName);
+    console.log(this.form.value.email);
+    console.log(this.form.value.region);
+    
+    
   }
+
+
+  // Side notes 
+  // When we check the form object in dev tools we'll look at two objects, controls and value
+  // controls: references the First Name element, Last Name element and so on in the form
+  // value: should have a property for each control and the value entered in that control
+
+  // FIRST To get the values and controls we need to add a name property to each of the form controls ie name="firstName"
+  // By adding a name property we are able to create the form controls in the NgForm object!!
+
+  // SECOND we need to add ngModel directive
+  // Usually used for 2 way databinding, here we use it to tell angular the input element is a control for this form
+  // In dev tools we can check the controls object, which not shows the controls which include the name reference and ngModel
+  // We can also check the value object, this holds the current value in the control
+
+  // The controls property gives us other properties we can access such as statusChanges, value, disabled, dirty and so on 
+  // value gives us access to just the value.
+  // Therefore, we have options on how to access a value or multiple properties
 }

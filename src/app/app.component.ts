41d1d8fa2618e2decd5596caf7fac5237f9d747b
Marco Torres <marcoantonio.torres@icloud.com>
2024-02-10 16:38:11 -0500

    In version 7, The focus was on adding some form validation. This was accomplished through the use of the required attribute to validate if there is any value in the form. For the email there is a need to add the email attribute, without email the form would take any value and see it as valid, with email the form needs a valid email for the entire form to be valid. I also added the disabled attribute with property binding syntx which allows us to completly disable the form if the valid property is false or if the invalid property is true.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,19 +20,40 @@ export class AppComponent {
 
   onFormSubmitted(){
     console.log(this.form);
-    // Accessing the values in the form 
-    console.log(this.form.control.value['firstName']);  // Instead of dot notation we use square bracket notation
+    console.log(this.form.control.value['firstName']); 
     console.log(this.form.value.lastName);
     console.log(this.form.value.email);
     console.log(this.form.value.region);
-    console.log(this.form.value);
+    
   }
 
-  // SIDE NOTES 
-  // Instead of repeating our buttons HTML 3 times, we can use ngFor directive to loop and show the genders
-  // First we create the genders array with 3 objects
-  // We can then go to the template and remove the divs to repeat one specific div for each gender
-  // On the first div I do property binding on id and name since we are assigning those values coming in into an attribute
-  // Then I can add string interpolation for the label
-  // That allows us to creating the input and labels dynamically
+  // SIDE NOTES FORM VALIDATION 
+  // In the template, if we enter values in the firstName, lastName, and email controls then the form will be valid (this includes the html 
+  // required attribute)
+
+  // If ANY of those fields/form control don't have a valid value then the field itself will be invalid and the entire form will be invalid
+  // This can be checked in with the invalid and valid properties of the form itself
+
+  // regarding form control... the invalid and valid will be validated based on the value, if there is validation and the user enters data
+  // that doesn't meet the validation then the form control will be invalid
+
+  // In a template driven form we add the code that will validate it in the template (duhhhhh)
+  // Once we add the required field, if we submit the form with no values or the wrong values, the form valid property will be false and
+  // the invalid property will be true. 
+  // Each form control also have their own valid and invalid properties satisfied by ANY values
+  
+  // This is bad since we can place a random value in the email field and it will satisfy the required property, since the required validator 
+  // property isn't looking to see if the email is correct, instead it checks if there is a value in the control
+  // To fix this we can use another html attribute called email. With this we now validate on an actual email as well.
+  // We can check this through the form control email valid and invalid property
+
+  // USE CASE: If the form is valid (if all form controls are valid) then enable the submit button and user can submit
+  // elseif that is false then disable the submit button
+  // To do this we first add the disabled property using property binding syntax. We then access the valid property through the template 
+  // reference variable. If we use valid property then we have the not operator in front !registrationForm.valid. If we have the invalid
+  // then it would look like [disabled]=registrationForm.invalid
+
+  // With dev tools we can inspect the app-root/section/form/div/div /div. With this we can see some CSS classes that are added
+  // These classes are ng-dirty, ng-valid and ng-touched, this will vary based on touching the control or putting values into the control
+  
 }

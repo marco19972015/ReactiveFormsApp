d17910aa78d9bb0a09f618c406f75ee45249af58
Marco Torres <marcoantonio.torres@icloud.com>
2024-02-12 14:42:57 -0500

    In version 8, the focus on this section was to add some custom validation to the form. This includes making the input box's border red, having an error message appear if the user clicked on the input filed and didn;t provide a valid value, and using two way data binding to show the user what they placed inside the input field.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,10 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'template-driven-form';
 
+  firstname: string = '';
+  lastname: string = '';
+  emailProperty: string = '';
+
   // Array with 3 objects 
   genders = [
     {id: 'check-male', value: 'male', display: 'Male'},
@@ -28,32 +32,55 @@ export class AppComponent {
   }
 
   // SIDE NOTES FORM VALIDATION 
-  // In the template, if we enter values in the firstName, lastName, and email controls then the form will be valid (this includes the html 
-  // required attribute)
+  // Going back to the CSS classes that are being added ng-touched/ng-dirty and ng-invalid/ng-valid
+  // We can use these CSS classes to our advantage. When there is some invalid data inside the first name input element we want to show 
+  // that border in red. 
 
-  // If ANY of those fields/form control don't have a valid value then the field itself will be invalid and the entire form will be invalid
-  // This can be checked in with the invalid and valid properties of the form itself
+  // First we want to create a CSS class to do this. The CSS class name must have ng-invalid because the ng-invalid CSS class gets added to
+  // the form control when it's not valid. We then add the ng-invalid {border: red px solid;} so when the class gets added it will also add the border
+  // NOTE that if we only have ng-invalid as the css class, when the ng-invalid is added we will get the border around the entire form
 
-  // regarding form control... the invalid and valid will be validated based on the value, if there is validation and the user enters data
-  // that doesn't meet the validation then the form control will be invalid
+  // In order to show the red border around the form control we need to change the CSS class a bit. 
+  // We add input.ng-invalid {...} to the class name - if it's input type and ng-invalid is added then we add the red border 
+  // NOTE that if we have a previous CSS border around the input tag, there might be an issue where it is being overriden, so remove that
 
-  // In a template driven form we add the code that will validate it in the template (duhhhhh)
-  // Once we add the required field, if we submit the form with no values or the wrong values, the form valid property will be false and
-  // the invalid property will be true. 
-  // Each form control also have their own valid and invalid properties satisfied by ANY values
+  // At this point the borders have the red border, HOWEVER now we want the border to change color to red when the user has touched it and the value isn't correct
+  // so we add... ng-touched... input.ng-invalid.ng-touched{...}
+  // Now, if we touch the control and click outside that control, the input field will add a red border. If we add the correct value, the red border will be removed
+
+
+
+  // Now, if the form control doesn't have a valid value we provide a custom validation message 
+  // First, we create a small tag and let the user know the field is required
+  // Second, we add a CSS class to the small tag, color: red, font-weight: bold;
+
+  // We don't want to show this error message always, we only want to show this error message when the user has touched the form control and there is no valid value
+  // For this we first add a template reference variable to the input tag called #fname.
+  // #fname now stores a reference to that specific input tag in the DOM (which returns us the input element in HTML)
+  // BUT, we want the input element as a JS object. 
+  // To fix this we can assign ngModel... #fname="ngModel" (we learned that when we use ngModel it tells ng that the input element is a form control for the form)
+  // Now it will store a reference of the input element as a JS object. 
+  // Doing this will give us access to the firstName property in the controls property. 
+  // #fname reference variable will be assigned firstName FormControl (can be seen in dev tool under controls)
+  // In this FormControl (firstName) it contains a valid and touched property that we can work with
+
+  // To the small tag we can use fname reference variable with the invalid and touched properties. 
+  // <small *ngIf="fname.invalid && fname.touched">*First Name is a Required Field</small>
+  // Initially the error message won't show, but if we click and don't put anything then the message will show
+  // Now we can add it to the lastName, and email input fields/controls
   
-  // This is bad since we can place a random value in the email field and it will satisfy the required property, since the required validator 
-  // property isn't looking to see if the email is correct, instead it checks if there is a value in the control
-  // To fix this we can use another html attribute called email. With this we now validate on an actual email as well.
-  // We can check this through the form control email valid and invalid property
 
-  // USE CASE: If the form is valid (if all form controls are valid) then enable the submit button and user can submit
-  // elseif that is false then disable the submit button
-  // To do this we first add the disabled property using property binding syntax. We then access the valid property through the template 
-  // reference variable. If we use valid property then we have the not operator in front !registrationForm.valid. If we have the invalid
-  // then it would look like [disabled]=registrationForm.invalid
 
-  // With dev tools we can inspect the app-root/section/form/div/div /div. With this we can see some CSS classes that are added
-  // These classes are ng-dirty, ng-valid and ng-touched, this will vary based on touching the control or putting values into the control
-  
+  // We can provide two way data binding so the user can see what is being placed in the form 
+  // In the TS file we create the firstName, lastName, and emailAddress
+  // In the template we use banana syntax on the ngModel and assign them the properties 
+  // We then use string interpolation to show the user what they have entered
+
+  // We don't want the "You entered: " text to show if the user hasn't entered anything. To fix this we...
+  // We use an ngIf statement in the template and place the property. If the property is empty, then don't show anything else
+  // show the text
+
+
+
+
 }
